790785b1aa4cfa27d99545fbcf2d8b2f2950b10a
Grushevich <mail.grushko@gmail.com>
2021-07-26 10:19:19 +0300

    6. Date. Отображение даты. [#310017 #181551]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -39,10 +39,4 @@ public class Item {
     public LocalDateTime getCreated() {
         return created;
     }
-
-    public static void main(String[] args) {
-    Item item = new Item();
-    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-    System.out.println(item.getCreated().format(formatter));
-    }
 }
